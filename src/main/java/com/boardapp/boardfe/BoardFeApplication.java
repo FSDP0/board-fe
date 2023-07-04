e4f40533032485d18bde2f924c6d50b21ce3d531
Ray Lee <fsdp0dev@gmail.com>
2023-07-04 17:12:57 +0900

    V 0.0.1 : Basic fe setting

--- src/main/java/com/boardapp/boardfe/BoardFeApplication.java
+++ src/main/java/com/boardapp/boardfe/BoardFeApplication.java
@@ -2,6 +2,7 @@ package com.boardapp.boardfe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class BoardFeApplication {
